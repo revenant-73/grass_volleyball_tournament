31e2e4ee3b648ae937d04eb86b51b12898249011
Loren <loren@example.com>
2026-04-01 17:01:09 -0700

    "fix-stripe-build-env"

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,10 +1,10 @@
 import Stripe from 'stripe'
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY is not set')
-}
+// We don't throw at the top level here to avoid crashing the build
+// if the env var isn't present during the build phase.
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY || ''
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2026-03-25.dahlia',
   typescript: true,
 })
